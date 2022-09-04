a5d7fcbd27b9315f4a641975532a11b745ce612c
ph-ubuntu-pc <phenriquelgsilva@gmail.com>
2022-09-04 20:09:21 -0300

    feat: block and unblock card

--- src/routers/cardRouter.ts
+++ src/routers/cardRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { activateCard, createCard, cardBalance } from "../controllers/cardController.js";
+import { activateCard, createCard, cardBalance, blockAndUnblockCard} from "../controllers/cardController.js";
 import { validateAPIKey } from "../middlewares/cardMiddleware.js";
 import { schemaValidator } from "../middlewares/schemaValidator.js";
 import { schemas } from "../schemas/schemas.js";
@@ -9,6 +9,8 @@ const cardRouter = Router();
 
 
 cardRouter.post("/newCard", validateAPIKey, schemaValidator(schemas.newCardSchema), createCard)
-cardRouter.patch("/activateCard",schemaValidator(schemas.activateCard),activateCard)
+cardRouter.patch("/activateCard",schemaValidator(schemas.activateCardSchema),activateCard)
 cardRouter.get("/cardBalance/:id", cardBalance)
+cardRouter.patch("/blockAndUnblockCard/:id", schemaValidator(schemas.blockAndUnblockCardSchema),blockAndUnblockCard)
+//cardRouter.patch("/unblockCard/:id", schemaValidator(schemas.blockAndUnblockCardSchema),unblockCard)
 export default cardRouter;
--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -8,9 +8,12 @@ export const schemas = {
     APIKeySchema: joi.object().keys({
       API_KEY: joi.string().required(),
     }),
-    activateCard: joi.object().keys({
+    activateCardSchema: joi.object().keys({
       cardId: joi.number().required(),
       securityCode: joi.string().required(),
+      password: joi.string().regex(/^\d+$/).length(4).required()
+    }),
+    blockAndUnblockCardSchema: joi.object().keys({
       password: joi.string().regex(/^\d+$/).length(4).required()
     })
   };
